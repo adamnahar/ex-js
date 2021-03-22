162aeb9b29e9864927ea7d0b6b5a2e822f1cc4df
adamnahar <adamnahar3020@gmail.com>
2021-03-22 14:04:20 +0100

    ex 1 a 4

---
+++ jour1.js
@@ -0,0 +1,26 @@
+console.log("hollo world!");
+//2
+var test = "my name is Adam";
+console.log(test);
+//3
+var name = 'Ali';
+console.log('Nice to meet you Ali');
+
+//4
+var test = "I'm very long !";
+console.log(test.length);
+
+//5
+
+var food = "croissant is meh";
+food = food.replace("meh", "so good");
+console.log(food);
+
+//6
+var basic = "This is Cool";
+var basicUp = basic.toUpperCase();
+var basicDown = basic.toLocaleLowerCase();
+
+console.log(basic);
+console.log(basicUp);
+console.log(basicDown);
