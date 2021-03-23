44b09d071bddae37f5a3d52182be273e9874c616
adamnahar <adamnahar3020@gmail.com>
2021-03-23 13:46:00 +0100

    joiur2 1 a 3

---
+++ jour2.js
@@ -0,0 +1,18 @@
+// 01 - Number
+
+var integer = 102;
+var float = 13.9;
+console.log(integer);
+console.log(float);
+
+// 02 - Convert
+
+var basic = 34;
+var stringified = basic.toString();
+console.log(stringified);
+
+// 03 - Round
+
+var num = 1.5;
+var rounded = Math.round(num);
+console.log(rounded);
