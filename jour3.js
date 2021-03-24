8414e97e675b9fb7c0dd9425dbd5197e9da4c68b
adamnahar <adamnahar3020@gmail.com>
2021-03-24 17:21:32 +0100

    jour3

--- jour3.js
+++ jour3.js
@@ -17,3 +17,30 @@ console.log(objects);
 objects.push("laptop");
 objects.shift();
 console.log(objects);
+
+//order
+var numbers = [4, 10, 8, 12, 6];
+numbers = numbers.reverse();
+console.log(numbers);
+numbers = numbers.sort((a, b) => a - b);
+console.log(numbers);
+
+//boucle
+
+var total = 0;
+var limit = 10;
+for (i = 0; i <= limit; i++) {
+  total = total + i;
+}
+console.log(total);
+
+//Reverse
+
+var string = "Hello Konexio !";
+var splitted = string.split("");
+var reversed = [];
+for (i = 0; i <= splitted.length; i++) {
+  reversed.unshift(splitted[i]);
+}
+var result = reversed.join("");
+console.log(result);
